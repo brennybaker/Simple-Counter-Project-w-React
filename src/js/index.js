d19ea1679ddb32f749b424d0f56aafe35b559603
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2023-04-11 19:37:24 +0000

    Clock and numbers: need to make counter work and fix styling issues

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,25 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
-
+function Counter(props){
+    return (<><div className = "bigContainer">
+                    <div className = "container1">
+                             <div className = "clock"><img src ="https://static.vecteezy.com/system/resources/previews/019/923/746/non_2x/clock-face-icon-black-and-white-transparent-background-free-png.png"></img></div>
+                    </div>
+                    <div className = "container2"> 
+                            <div className = "first">{props.firstDigit}</div>
+                            <div className = "second">{props.secondDigit}</div>
+                            <div className = "third">{props.thirdDigit}</div>
+                            <div className = "fourth">{props.fourthDigit}</div>
+                            <div className = "fifth">{props.fifthDigit}</div>
+                            <div className = "sixth">{props.sixthDigit}</div>
+                    </div>
+            </div></>
+            )
+}
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function(){
+ReactDOM.render(<Counter firstsDigit />, document.querySelector("#app")
+);
+},1000);
